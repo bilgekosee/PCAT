998a55fcbe99ae0a5db15d7d6c43f51e8f641345
bilgekosee <bilge.koosee@gmail.com>
2025-02-02 14:22:46 +0300

    Fix duplicate res.render call and add error handling

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const path = require('path');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+mongoose
+  .connect('mongodb://localhost/pcat-demo-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB bağlantısı başarılı'))
+  .catch((err) => console.log('MongoDB bağlantı hatası:', err));
+
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 //MIDDLEWARE
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 const port = 3000;
 app.listen(port, () => {
